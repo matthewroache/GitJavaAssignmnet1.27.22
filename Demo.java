acc9c647542c7b0584680c95e99147b16a9f3937
mattjro <roachemj@appstate.edu>
2023-01-27 14:43:24 -0500

    Updated Demo

--- Demo.java
+++ Demo.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
 public class Demo 
 {
     public static void main(String[] args) 
     {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Please choose a fruit, either Apple or Mango: ");
+
+        String userInput = kb.nextLine();
+        System.out.println("You chose " + userInput);
+
+        Fruits apple = new Apple();
+        Fruits mango = new Mango();
+        String ret;
+        if(userInput.equals("Apple"))
+        {
+            ret = apple.getColor();
+            System.out.println("The color of an apple is " + ret);
+        }
+        else if (userInput.equals("Mango"))
+        {
+            ret = mango.getColor();
+            System.out.println("The color of an apple is " + ret);
+        }
+
+
         
     }
     
