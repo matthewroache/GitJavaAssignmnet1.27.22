58816f75d8a92defa210e024259a615ffbd0a4e7
mattjro <roachemj@appstate.edu>
2023-01-27 14:49:23 -0500

    Completed program

--- Demo.java
+++ Demo.java
@@ -7,23 +7,27 @@ public class Demo
         System.out.println("Please choose a fruit, either Apple or Mango: ");
 
         String userInput = kb.nextLine();
-        System.out.println("You chose " + userInput);
-
         Fruits apple = new Apple();
         Fruits mango = new Mango();
         String ret;
         if(userInput.equals("Apple"))
         {
             ret = apple.getColor();
+            System.out.println("You chose " + userInput);
+
             System.out.println("The color of an apple is " + ret);
         }
         else if (userInput.equals("Mango"))
         {
             ret = mango.getColor();
+            System.out.println("You chose " + userInput);
+
             System.out.println("The color of an apple is " + ret);
         }
-
-
+        else
+        {
+            System.out.println("You need to enter the keyword 'Apple' or 'Mango'.");
+        }
         
     }
     
